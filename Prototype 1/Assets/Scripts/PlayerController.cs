205360fe63a287371a8624b239982a85ed79a937
deloxi <52937935+deloxi@users.noreply.github.com>
2022-06-10 17:26:01 +0200

    Lesson 1.4
    Camera movement, player input for car movement

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float turnSpeed = 40.0f;
+    private float speed = 10.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +18,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // We'll move the vehicle forward
-        transform.Translate(Vector3.forward* Time.deltaTime * 20);
+        // player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // We move the vehicle forward
+        transform.Translate(Vector3.forward* Time.deltaTime * speed * forwardInput);
+
+        // We move vehicle sideways
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
