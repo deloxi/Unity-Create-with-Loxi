498bc30c842f79966c67c12707eebc8a8c8d8264
deloxi <52937935+deloxi@users.noreply.github.com>
2022-06-29 18:08:17 +0200

    Unit 3
    Started unit 3

---
+++ Unit 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float dogSpawnCooldown = 1.0f;
+    public float recentDogSpawn;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && (Time.time - recentDogSpawn > dogSpawnCooldown))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            recentDogSpawn = Time.time;
+        }
+    }
+}
